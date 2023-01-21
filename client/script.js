bc6447d0c4e0ac3d9d0232ff3645c0c7a09a8d7c
valory <renatacallef@gmail.com>
2023-01-20 22:02:38 -0500

    url fixed(?)

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // Fetch data from server -> bot's response
-  const response = await fetch('https://botki.onrender.com/',{
+  const response = await fetch('https://botki.onrender.com',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
